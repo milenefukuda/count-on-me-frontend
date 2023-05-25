b43ab26ee1c5463c0d0c9da2d05c718dff3f555b
Milene Fukuda <milene.fukuda@gmail.com>
2023-05-25 10:53:09 +0100

    event feed and tailwind

---
+++ src/components/EventFeed/index.js
@@ -0,0 +1,32 @@
+import { api } from "../../api/api.js";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export function EventFeed(props) {
+  const { search } = props;
+  const [eventFeed, setEventFeed] = useState([]),
+    [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getAllEvents() {
+      try {
+        const response = await api.get("/all-events");
+        setEventFeed(response.data);
+        setIsLoading(false);
+        console.log(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getAllEvents();
+  }, []);
+  console.log(EventFeed);
+
+  return (
+    <>
+      <div>
+        <h1>MAP PARA RENDERIZAR TODOS OS EVENTOS</h1>
+      </div>
+    </>
+  );
+}
