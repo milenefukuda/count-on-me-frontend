8378e52f10cd4e2f96b26ca43a7a1cee2272f812
Milene Fukuda <milene.fukuda@gmail.com>
2023-06-06 13:53:03 +0100

    App file updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Signup } from "./pages/Signup/index.js";
 import { Home } from "./pages/Home/index.js";
 import { About } from "./pages/About.js";
+import { Login } from "./pages/Login.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
