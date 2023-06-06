eb823e51b1a072c7c51c12237d82979e5d21f645
Milene Fukuda <milene.fukuda@gmail.com>
2023-06-06 19:23:42 +0100

    navbar bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import { AuthContextComponent } from "./contexts/authContext.js";
 import { Signup } from "./pages/Signup/index.js";
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,19 +1,50 @@
 import { Link } from "react-router-dom";
+import { SearchBar } from "../SearchBar";
+import { useState } from "react";
+import { FaFistRaised, FaUserAlt, FaSearch } from "react-icons/fa";
 
 export function NavBar() {
-  const NavBtnClass = "flex flex-row intems-center justifify-center";
+  const [search, setSearch] = useState("");
+  function changeSearch(text) {
+    setSearch(text);
+  }
+
   return (
     <>
-      <nav className="h-16 text-lg border-b-2 border-indigo-400 flex flex-row items-center justify-evenly p-2 bg-indigo-700 mb-8 font-semibold text-indigo-100 shadow-xl">
-        <Link to="/" className={NavBtnClass}>
-          <button>HOME ÍCONE</button>
-        </Link>
-        <Link to="/about" className={NavBtnClass}>
-          <button>ABOUT ÍCONE</button>
-        </Link>
-        <Link to="/signup" className={NavBtnClass}>
-          <button>SIGNUP ÍCONE</button>
-        </Link>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <FaFistRaised />
+            You count on me!
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <SearchBar changeSearch={changeSearch} />
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/login"
+                >
+                  <FaUserAlt />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </>
   );
