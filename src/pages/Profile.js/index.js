db3b8ecdef541bb2de79c5a8ce87408462824f29
Milene Fukuda <milene.fukuda@gmail.com>
2023-06-09 14:37:29 +0100

    profile file updated

--- src/pages/Profile.js/index.js
+++ src/pages/Profile.js/index.js
@@ -1,7 +1,12 @@
+import { NavBar } from "../../components/NavBar";
+import { Footer } from "../../components/Footer";
+
 export function Profile() {
   return (
     <>
+      <NavBar />
       <h1>Seu perfil aqui</h1>
+      <Footer />
     </>
   );
 }
