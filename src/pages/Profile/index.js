7643e0b792bface218894aee1e034c0545f288d1
Milene Fukuda <milene.fukuda@gmail.com>
2023-06-21 23:24:09 +0100

    profile log out and link to create event

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,7 +2,7 @@ import { api } from "../../api/api.js";
 import { NavBar } from "../../components/NavBar";
 import { Footer } from "../../components/Footer";
 import { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/authContext.js";
 
 export function Profile() {
@@ -31,18 +31,27 @@ export function Profile() {
       <NavBar />
       <div className="container">
         <div className="row">
-          <div className="col-md-6 d-flex align-items-center justify-content-start">
-            <h1>{`Hello, ${user.name}`}</h1>
+          <div className="col-md-6 d-flex align-items-center justify-content-center flex-column">
+            <h1 style={{ color: "#000000", fontSize: "24px" }}>
+              Hello, {user.name}
+            </h1>
+            <button className="btn btn-primary mt-2" onClick={handleLogOut}>
+              Log Out
+            </button>
+            <Link to="/event/create" className="btn btn-primary mt-2">
+              Create Event
+            </Link>
           </div>
-          <img
-            src="https://res.cloudinary.com/dieqaoy0n/image/upload/v1686140455/mundo_desktop_hvwx73.png"
-            alt="Imagem"
-            className="img-fluid"
-            style={{ maxWidth: "100%" }}
-          />
+          <div className="col-md-6">
+            <img
+              src="https://res.cloudinary.com/dieqaoy0n/image/upload/v1686140455/mundo_desktop_hvwx73.png"
+              alt="Imagem"
+              className="img-fluid"
+              style={{ maxWidth: "100%" }}
+            />
+          </div>
         </div>
       </div>
-      <button onClick={handleLogOut}>Log Out</button>
       <Footer />
     </>
   );
