a276d4b048e95f357e1a5baf85e9da5d3b32fa6a
Milene Fukuda <milene.fukuda@gmail.com>
2023-06-21 22:11:33 +0100

    create event page updated

--- src/pages/CreateEvent/index.js
+++ src/pages/CreateEvent/index.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { api } from "../../api/api.js";
+import { NavBar } from "../../components/NavBar.index.js";
+import { Footer } from "../../components/Footer.index.js";
 
 export function CreateEvent() {
   const navigate = useNavigate;
@@ -11,4 +14,28 @@ export function CreateEvent() {
     picture: "",
     description: "",
   });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const clone = { ...form };
+
+    try {
+      const response = await api.post("/event/create", { ...clone });
+      localStorage.setItem("loggedInUser", JSON.stringify(response.data));
+      navigate("/event/:id");
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
+
+return (
+  <>
+    <NavBar />
+    <Footer />
+  </>
+);
